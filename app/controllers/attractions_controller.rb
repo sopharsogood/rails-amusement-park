da33c67060c33db01e486df80c28ded6bcd9bcbd
sophie <justanothersophie@gmail.com>
2021-09-16 14:53:52 -0400

    Completed lab, all tests passing

--- app/controllers/attractions_controller.rb
+++ app/controllers/attractions_controller.rb
@@ -4,7 +4,39 @@ class AttractionsController < ApplicationController
         @attractions = Attraction.all
     end
 
+    def new
+        @attraction = Attraction.new
+    end
+
     def show
         @attraction = Attraction.find(params[:id])
+    end
+
+    def edit
+        @attraction = Attraction.find(params[:id])
+    end
+
+    def create
+        @attraction = Attraction.new(attraction_params)
+        if @attraction.save
+            redirect_to attraction_path(@attraction)
+        else
+            render :new
+        end
+    end
+
+    def update
+        @attraction = Attraction.find(params[:id])
+        if @attraction.update(attraction_params)
+            redirect_to attraction_path(@attraction)
+        else
+            render :edit
+        end
+    end
+
+    private
+    
+    def attraction_params
+        params.require(:attraction).permit(:name, :tickets, :happiness_rating, :nausea_rating, :min_height)
     end
 end
